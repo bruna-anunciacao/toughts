f14c1d0773d1c49b171fb2745cdbc550afc484b3
bruna-anunciacao <bruunaaa04@gmail.com>
2023-09-01 18:51:22 -0300

    creating a user registration function

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,45 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render('auth/login')
     }
     static register(req, res) {
         res.render('auth/register')
+    }
+    static async registerPost(req, res) {
+        const { name, email, password, confirmpassword } = req.body;
+        if (password != confirmpassword){
+            req.flash('message', 'As senhas não são iguais');
+            res.render('auth/register');
+            return;
+        }
+        const checkIfUserExists = await User.findOne({ where: { email } });
+        if (checkIfUserExists){
+            req.flash('message', 'Email já cadastrado');
+            res.render('auth/register');
+            return;
+        }
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        const user = {
+            name,
+            email,
+            password: hash
+        }
+        try {
+            const createdUser = await User.create(user);
+            req.session.userid = createdUser.id;
+
+            req.flash('message', 'Usuário cadastrado com sucesso');
+            req.session.save(() => {
+                res.redirect('/');
+            })
+        } catch (error) {
+            req.flash('message', 'Erro ao cadastrar usuário');
+            res.render('auth/register');
+        }
     }
 }
