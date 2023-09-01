532e100d0ea94cc660fb1e8eb6f53487ac99b810
bruna-anunciacao <bruunaaa04@gmail.com>
2023-09-01 13:35:25 -0300

    finilizing initial setup

--- index.js
+++ index.js
@@ -8,7 +8,46 @@ const app = express();
 
 const conn = require("./db/conn");
 
+app.engine('handlebars', exphbs.engine());
+app.set("view engine", "handlebars");
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(
+  session({
+    name: "session",
+    secret: "my-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(flash())
+
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  if (req.session.userid){
+    res.locals.session = req.session;
+  }
+  next();
+})
 
 conn
   .sync()
