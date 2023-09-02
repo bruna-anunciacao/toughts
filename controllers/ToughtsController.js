91e243a67babd76308c728297a0652260aa63643
bruna-anunciacao <bruunaaa04@gmail.com>
2023-09-02 13:02:30 -0300

    inserting toughts into the database

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -1,14 +1,29 @@
-const Toughts = require('../models/Toughts')
-const User = require('../models/User')
+const Toughts = require("../models/Toughts");
+const User = require("../models/User");
 
 module.exports = class ToughtsController {
-    static async showToughts(req, res) {
-        res.render('toughts/home')
+  static async showToughts(req, res) {
+    res.render("toughts/home");
+  }
+  static async profile(req, res) {
+    res.render("toughts/profile");
+  }
+  static createToughts(req, res) {
+    res.render("toughts/create");
+  }
+  static async createToughtsSave(req, res) {
+    const tought = {
+      title: req.body.title,
+      UserId: req.session.userid,
+    };
+    try {
+      await Toughts.create(tought);
+      req.flash("message", "Pensamento criado!");
+      req.session.save(() => {
+        res.redirect("/toughts/profile");
+      });
+    } catch (err) {
+      console.log(err);
     }
-    static async profile(req, res) {
-        res.render('toughts/profile')
-    }
-    static createToughts(req, res) {
-        res.render('toughts/create')
-    }
-}
+  }
+};
--- routers/toughtsRouters.js
+++ routers/toughtsRouters.js
@@ -5,6 +5,7 @@ const ToughtsController = require("../controllers/ToughtsController");
 const checkAuth = require("../helpers/auth").checkAuth;
 
 router.get('/add', checkAuth, ToughtsController.createToughts);
+router.post('/add', checkAuth, ToughtsController.createToughtsSave);
 router.get('/profile', checkAuth, ToughtsController.profile);
 router.get("/", ToughtsController.showToughts);
 
